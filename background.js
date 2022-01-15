0850c48016998f45831163439112989f3e7d36d7
Jon Dewey <donjewey@gmail.com>
2022-01-14 16:38:33 -0800

    jan14

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.browserAction.onClicked.addListener(function () {
+chrome.action.onClicked.addListener(function () {
     chrome.tabs.create({
-        'url': chrome.extension.getURL('popup.html')
+        'url': chrome.extension.getURL('index.html')
     }, function (tab) {
         // Tab opened.
     });
--- main.js
+++ main.js
@@ -9,67 +9,132 @@
 // categories??
 
 document.addEventListener("DOMContentLoaded", () => {
+    const border = document.getElementById('border')
+    border.setAttribute('id', 'border')
+    const body = document.body
+
+    const items = []
+
+    body.appendChild(border)
+
     // grid-like, borders around each rectangle inside outer border
-    //const puppeteer = require("puppeteer");
-
-    const title = document.createElement("h1");
-    const border = document.createElement("div");
-
-    // amazon ID [always 10 chars, caps or nums]
-    let url = "";
-
-    title.setAttribute("id", "title");
-    border.setAttribute("id", "border");
-
-    title.innerText = "SHOPLIST";
-
     const shoppingListIcon = document.createElement("img");
     shoppingListIcon.src = "https://static.thenounproject.com/png/166411-200.png";
     shoppingListIcon.setAttribute("id", "shopicon");
-
-    // appending elements
-    document.body.appendChild(title);
-    document.body.appendChild(border);
 
     // array of objects,
     // object format: name, price, image, url
     const list = [];
     let tempItemObj = {}
 
+    // for entering new item
+    const nameBox = document.createElement("input");
+    nameBox.setAttribute('class', 'input');
+    nameBox.setAttribute('placeholder', 'Enter Item Name');
+    border.appendChild(nameBox);
 
-    // dom layout to manually add item
-    
+    border.appendChild(document.createElement("br"))
 
+    const priceBox = document.createElement("input");
+    priceBox.setAttribute('class', 'input');
+    priceBox.setAttribute('placeholder', 'Enter Item Price');
+    border.appendChild(priceBox);
 
+    border.appendChild(document.createElement("br"))
 
+    const urlBox = document.createElement("input");
+    urlBox.setAttribute('class', 'input');
+    urlBox.setAttribute('placeholder', 'Enter Item URL');
+    border.appendChild(urlBox);
 
-    // for pasting url of new item
-    const inputBox = document.createElement("input");
-    inputBox.setAttribute('placeholder', 'Enter Item URL');
-    border.appendChild(inputBox);
+    border.appendChild(document.createElement("br"))
 
-    // args: item object (see list comments on format)
-    // adds new item to end of list
-    const appendList = function (item) {
+    const submit = document.createElement("button")
+    submit.setAttribute('id', 'submitButton')
+    border.appendChild(submit)
+    submit.innerText = "Add to List"
+
+    submit.addEventListener("click", ()=>{
+        let tempItemObj = {}
+        tempItemObj.name = nameBox.value
+        tempItemObj.price = priceBox.value
+        tempItemObj.url = urlBox.value
+
+        nameBox.value = ''
+        priceBox.value = ''
+        urlBox.value = ''
+        
+        items.push(tempItemObj)
+    })
+
+    //setInterval(displayList)
+
+    // args: items array
+    // displays list of items
+    const displayList = function (arr) {
         const newItem = document.createElement("div");
         newItem.setAttribute("id", `item${list.length}`);
         newItem.setAttribute("class", "item");
-        const itemImage = document.createElement("img");
-        itemImage.setAttribute("id", "itemImage")
-        itemImage.setAttribute("src", item.image);
+        // const itemImage = document.createElement("img");
+        // itemImage.setAttribute("id", "itemImage")
+        // itemImage.setAttribute("src", item.image);
         const itemName = document.createElement("h2");
         itemName.setAttribute("id", "itemName");
         const itemPrice = document.createElement("h3")
         itemPrice.setAttribute("id", "itemPrice");
 
         border.appendChild(newItem);
-        newItem.appendChild(itemImage);
+        // newItem.appendChild(itemImage);
         newItem.appendChild(itemName);
         newItem.appendChild(itemPrice);
 
         itemName.innerHTML = item.name;
         itemPrice.innerHTML = item.price;
     };
+
+    // Style Sheet
+    const styles = {
+        "body":{
+            "background-color": "#242424"
+        },
+        "#border": {
+            "text-align": "center",
+            "border": "2px solid white",
+            "height": "75%",
+            "width": "50%"
+        },
+        "#submitButton":{
+            "margin": "auto",
+            "text-align": "center"
+        },
+        ".input": {
+            "margin": "auto",    
+            "text-align": "center",
+            "width": "95%"
+        },
+        ".item": {
+            "border": "1px solid white",
+            "position": "relative",
+            "width": "95%",
+            "height": "10%",
+        },
+        ".qarstb": {
+            "display": "flex",
+            "align-items": "center",
+            "justify-content": "center",
+        },
+        "#shopicon": {
+            "height": "200px",
+            "border": "1px solid black",
+        },
+    };
+
+    for (const [node, style] of Object.entries(styles)) {
+        const nodesToBeStyled = document.querySelectorAll(node);
+        nodesToBeStyled.forEach((currentNode) => {
+            Object.assign(currentNode.style, style);
+        });
+    }
 
     const getItem = function (url) {
         if (url) {
@@ -102,36 +167,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 inputBox.innerText = ''
             }
         })
-
-        // Style Sheet
-        const styles = {
-            body: {},
-            "#border": {
-                "border": "2px solid white",
-            },
-            ".item": {
-                "border": "1px solid white",
-                "position": "relative",
-                "width": "95%",
-                "height": "10%",
-            },
-            ".qarstb": {
-                "display": "flex",
-                "align-items": "center",
-                "justify-content": "center",
-            },
-            "#shopicon": {
-                "height": "200px",
-                "border": "1px solid black",
-            },
-        };
-
-        for (const [node, style] of Object.entries(styles)) {
-            const nodesToBeStyled = document.querySelectorAll(node);
-            nodesToBeStyled.forEach((currentNode) => {
-                Object.assign(currentNode.style, style);
-            });
-        }
     }
 });
 //<div class="o3j99 qarstb"><style data-iml="1642125379303">.vcVZ7d{text-align:center}</style></div>
